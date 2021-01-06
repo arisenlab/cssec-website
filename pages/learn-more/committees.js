6db3f82ba1344ec59ff6112b133455352ac08981
Bryce Philbert Salvador <bpmsalvador@addu.edu.ph>
2021-01-06 14:07:06 +0800

    Added components and assets for committees page

--- pages/learn-more/committees.js
+++ pages/learn-more/committees.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Ccard from "../../components/compCom/Ccard";
+
+import committees from "../../data/committees";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: "98%",
+        margin: "auto",
+    },
+    redirects: {
+        display: "flex",
+        flexWrap: "wrap",
+    },
+}));
 
 const Committees = () => {
-    return <div>Committees</div>;
+    const classes = useStyles();
+    return <div>
+         <Grid container spacing={2} style={{ marginTop: 10 }}>
+            {committees.map(image => {
+                return (
+                    <Grid item xs={4}>
+                        <Ccard
+                            imgSrc={image.url}
+                            title={image.title}
+                            description={image.description}
+                        />
+                    </Grid>
+                );
+             })}
+        </Grid>       
+    </div>;
 };
 
 export default Committees;
